b02b7718096b5a386bf37796a6031db22063dbac
yoonhyoyeon <loveyhy2002@naver.com>
2024-11-13 23:16:52 +0900

    container/GraduationDetail styled

---
+++ src/component/Table/index.jsx
@@ -0,0 +1,30 @@
+import styles from './index.module.css';
+import TableRow from '@/component/TableRow';
+
+const Table = ({data}) => {
+    return (
+        <div className={styles.container}>
+            <TableRow
+                code="학수번호"
+                name="과목명"
+                type="이수구분"
+                sector="선택영역"
+                credit="학점"
+                checked={false}
+            />
+            { data.map((v, i) => (
+                <TableRow 
+                    key={i}
+                    code={v.code} 
+                    name={v.name} 
+                    type={v.type}
+                    sector={v.sector}
+                    credit={v.credit}
+                    checked={v.checked}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default Table;
