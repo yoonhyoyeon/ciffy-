2355bd04823ade5f0206ce0c15a36e3c4d6d23c3
yoonhyoyeon <loveyhy2002@naver.com>
2024-12-01 08:28:04 +0900

    vercel build error: useSearchParams 해결#2

--- src/component/Navigation/index.jsx
+++ src/component/Navigation/index.jsx
@@ -1,7 +1,7 @@
 "use client"
 import styles from './index.module.css';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import Button from '@/component/Button';
 import LoginPopup from '@/component/LoginPopup';
@@ -56,42 +56,45 @@ const Navigation = () => {
     }, []);
 
     return (
-        <div style={background ? backgroundStyle : null} className={`${styles.container} ${mobile_opened ? styles.opened : null}`}>
-            { mobile_opened ? <div onClick={() => setMobile_opened((prev)=>!prev)} className={styles.mobile_background}></div> : null}
-            <div className={styles.mobile_navbar}>
-                <Link href="/"><img className={styles.logo} src="/images/logo.png" /></Link>
-                <div
-                    className={`${styles.menu_btn} ${mobile_opened ? styles.opened : null}`} 
-                    onClick={() => setMobile_opened((prev)=>!prev)}
-                >
-                    <div className={styles.bar1}></div>
-                    <div className={styles.bar2}></div>
-                    <div className={styles.bar3}></div>
-                </div>
-            </div>
-            <div className={styles.leftarea}>
-                <Link href="/"><img className={styles.logo} src="/images/logo.png" /></Link>
-                <ul className={styles.navlist}>
-                    <Link onClick={() => setMobile_opened((prev)=>!prev)} href="/timetable/create/survey"><li className={path.startsWith('/timetable/create')?styles.active:null}>시간표짜기</li></Link>
-                    <Link onClick={() => setMobile_opened((prev)=>!prev)} href="/lecture"><li className={path.startsWith('/lecture')?styles.active:null}>강의후기</li></Link>
-                    <Link onClick={() => setMobile_opened((prev)=>!prev)} href="/graduation"><li className={path.startsWith('/graduation')?styles.active:null}>졸업요건분석</li></Link>
-                </ul>
-            </div>
-            <div className={styles.rightarea}>
-                { isAuthorized===null ? null : isAuthorized ?
-                    <UserDropdown
-                        setBackground={setBackground}
-                    /> :
-                    <Button 
-                        size="small"
-                        onClick={openLogin}
+        <Suspense>
+            <div style={background ? backgroundStyle : null} className={`${styles.container} ${mobile_opened ? styles.opened : null}`}>
+                { mobile_opened ? <div onClick={() => setMobile_opened((prev)=>!prev)} className={styles.mobile_background}></div> : null}
+                <div className={styles.mobile_navbar}>
+                    <Link href="/"><img className={styles.logo} src="/images/logo.png" /></Link>
+                    <div
+                        className={`${styles.menu_btn} ${mobile_opened ? styles.opened : null}`} 
+                        onClick={() => setMobile_opened((prev)=>!prev)}
                     >
-                        로그인
-                    </Button>
-                }
+                        <div className={styles.bar1}></div>
+                        <div className={styles.bar2}></div>
+                        <div className={styles.bar3}></div>
+                    </div>
+                </div>
+                <div className={styles.leftarea}>
+                    <Link href="/"><img className={styles.logo} src="/images/logo.png" /></Link>
+                    <ul className={styles.navlist}>
+                        <Link onClick={() => setMobile_opened((prev)=>!prev)} href="/timetable/create/survey"><li className={path.startsWith('/timetable/create')?styles.active:null}>시간표짜기</li></Link>
+                        <Link onClick={() => setMobile_opened((prev)=>!prev)} href="/lecture"><li className={path.startsWith('/lecture')?styles.active:null}>강의후기</li></Link>
+                        <Link onClick={() => setMobile_opened((prev)=>!prev)} href="/graduation"><li className={path.startsWith('/graduation')?styles.active:null}>졸업요건분석</li></Link>
+                    </ul>
+                </div>
+                <div className={styles.rightarea}>
+                    { isAuthorized===null ? null : isAuthorized ?
+                        <UserDropdown
+                            setBackground={setBackground}
+                        /> :
+                        <Button 
+                            size="small"
+                            onClick={openLogin}
+                        >
+                            로그인
+                        </Button>
+                    }
+                </div>
+                <LoginPopup opened={loginOpened} setOpened={setLoginOpened}/>
             </div>
-            <LoginPopup opened={loginOpened} setOpened={setLoginOpened}/>
-        </div>
+        </Suspense>
+        
     );
 }
 
